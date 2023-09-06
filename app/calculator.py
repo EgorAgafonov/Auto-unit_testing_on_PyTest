900404dd3f8b665748981c932427346a4a934de4
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2023-09-06 10:07:58 +0300

    Files updated

---
+++ app/calculator.py
@@ -0,0 +1,16 @@
+""" Модуль с классом Calculator."""
+
+class Calculator:
+    """Класс Calculator с функциями сложения, вычитания, умножения и деления чисел."""
+
+    def multiply(self, x, y):
+        return x * y
+
+    def division(self, x, y):
+        return x / y
+
+    def subtraction(self, x, y):
+        return x - y
+
+    def adding(self, x, y):
+        return x + y
